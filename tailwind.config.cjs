e9aded785f1d6b5c9e406d68e2aa6a1a25418c7d
8rxn <rajaryanvishwakarma10@gmail.com>
2023-02-04 23:05:27 +0530

    added daisy ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  themes: ["luxury", "autumn"],
+
+  plugins: [require("daisyui")],
+};
