7a27ac032c702442714c481b72d21176bc1ce413
8rxn <rajaryanvishwakarma10@gmail.com>
2023-02-06 15:56:28 +0530

    Feat: Arcana Installed and set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Auth, useAuth } from "@arcana/auth-react";
+
+
+
+const onLogin = () => {
+  // Route to authenticated page
+}
 
 const App = () => {
+  
+  const auth = useAuth();
   return (
-    <div> 
+    <div>
+      {auth.loading ? (
+        "Loading"
+      ) : auth.isLoggedIn ? (
+        <p>Logged In</p>
+      ) : (
+        <div>
+          <Auth externalWallet={true} theme={"dark"} onLogin={onLogin}/>
+        </div>
+      )}
       <div className="mockup-window border bg-base-300">
-  <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
-</div>
+        <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+      </div>
     </div>
   )
 }
