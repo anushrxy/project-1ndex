e85a0724ecae8c5d443d9647468d17a17a7fc485
Anushrey Jain <anushrxy@gmail.com>
2023-02-10 01:43:48 +0530

    updated arcana

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { AuthProvider } from "@arcana/auth";
 import { ProvideAuth } from "@arcana/auth-react";
 import { BrowserRouter } from 'react-router-dom';
 
-const provider = new AuthProvider(`aed154c1721d2970239f33c6045f27ba51b3ad6c`)
+const provider = new AuthProvider(`c7d67b8c243fc2be269f188ce225ce18472e1dc4`)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
