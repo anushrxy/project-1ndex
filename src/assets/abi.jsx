1d522c775d7ed35d19ea13180dfa8bccc52c0e20
Anushrey Jain <anushrxy@gmail.com>
2023-02-10 23:03:32 +0530

    arcana update

---
+++ src/assets/abi.jsx
@@ -0,0 +1,97 @@
+export const abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			}
+		],
+		"name": "addName",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "showBal",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "payable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "greet",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			}
+		],
+		"name": "Greet",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "myNumber",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "names",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { AuthProvider } from "@arcana/auth";
+import { AuthProvider, CHAIN } from "@arcana/auth";
 import { ProvideAuth } from "@arcana/auth-react";
 import { BrowserRouter } from 'react-router-dom';
 
-const provider = new AuthProvider(`c7d67b8c243fc2be269f188ce225ce18472e1dc4`)
+const appAddress = 'c7d67b8c243fc2be269f188ce225ce18472e1dc4'
+const auth = new AuthProvider(`${appAddress}`, {
+  network: 'testnet', 
+  position: 'right', 
+  theme: 'dark', 
+  alwaysVisible: true, 
+  chainConfig: {
+    chainId: CHAIN.POLYGON_MUMBAI_TESTNET, 
+    rpcUrl: 'https://fragrant-greatest-sky.matic-testnet.discover.quiknode.pro/51a324530a248242144de9b79fc28e6f2272e30d/', 
+  },
+})
+
+try {
+  await auth.init()
+} catch (e) {
+  console.log(e);
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ProvideAuth provider={provider}>
+    <ProvideAuth provider={auth}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
