17d7ae041dc4cca8c9652019913b665bae07aa20
Anushrey Jain <anushrxy@gmail.com>
2023-02-13 00:23:08 +0530

    quicknode added

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 import { AuthProvider, CHAIN } from "@arcana/auth";
 import { ProvideAuth } from "@arcana/auth-react";
 import { BrowserRouter } from 'react-router-dom';
-// import Web3 from "web3"
 
 const appAddress = 'c7d67b8c243fc2be269f188ce225ce18472e1dc4'
 const auth = new AuthProvider(`${appAddress}`, {
@@ -15,20 +14,14 @@ const auth = new AuthProvider(`${appAddress}`, {
   alwaysVisible: false, 
   chainConfig: {
     chainId: CHAIN.POLYGON_MUMBAI_TESTNET, 
-    rpcUrl: 'https://polygon-mumbai.g.alchemy.com/v2/Ifo0nkR33bwWSq-EsiCaY0V-MyRtqcM8',
+    rpcUrl: 'https://fragrant-greatest-sky.matic-testnet.discover.quiknode.pro/51a324530a248242144de9b79fc28e6f2272e30d/',
     },
 })
-
-auth.init();
-
-
-// useEffect(()=>{
-//   {async function arcanaInit() {
-//   await auth.init();
-// }
-    
-//     arcanaInit();}
-// },[])
+async function initialise() {
+  await auth.init();
+  console.log('initialise')
+}
+initialise();
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
