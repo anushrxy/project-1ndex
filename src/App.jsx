a2df7529e96c45392bbef5cc759c0ed0fbe865ee
Ashutosh <ashutosh_rath@users.noreply.github.com>
2023-02-14 16:05:14 +0530

    feat: add dashboard and transaction form

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import NavBarFirst from "../src/components/NavBar.first"
-import { Routes, Route } from 'react-router-dom';
-import { Home, Login } from './pages';
-import { useAuth } from '@arcana/auth-react';
-import { ethers } from 'ethers';
+import React, { useEffect, useState } from "react";
+import NavBarFirst from "../src/components/NavBar.first";
+import { Routes, Route } from "react-router-dom";
+import { Home, Login } from "./pages";
+import { useAuth } from "@arcana/auth-react";
+import { ethers } from "ethers";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Transaction from "./components/Transactions/Transaction";
 
 const App = () => {
-  const[connectedAdd,setConnectedAdd] = useState("");
-  const[balance,setBalance] = useState(0);
+  const [connectedAdd, setConnectedAdd] = useState("");
+  const [balance, setBalance] = useState(0);
 
-  const exec = async()=>{
+  const exec = async () => {
     const auth = useAuth();
-    const arcanaProvider = await auth.connect()
-    const provider = new ethers.providers.Web3Provider(arcanaProvider)
+    const arcanaProvider = await auth.connect();
+    const provider = new ethers.providers.Web3Provider(arcanaProvider);
     setConnectedAdd(await provider.send("eth_requestAccounts", []));
-    let balance = await provider.getBalance(connectedAdd[0])
+    let balance = await provider.getBalance(connectedAdd[0]);
     let balanceInEth = ethers.utils.formatEther(balance);
     setBalance(balanceInEth);
-    console.log(balance)
-    return balance
+    console.log(balance);
+    return balance;
+  };
 
-}
-
-// useEffect(()=>{
-//   exec().then((data)=>{
-//     console.log(data)
-//   })
-// },[])
+  // useEffect(()=>{
+  //   exec().then((data)=>{
+  //     console.log(data)
+  //   })
+  // },[])
   return (
-    <div className='overflow-hidden'>
-      <NavBarFirst/>
+    <div className="overflow-hidden">
+      <NavBarFirst />
+      <Dashboard />
+      <Transaction />
       <Routes>
-        <Route path='/login' element={<Login/>} />
-        <Route path='/' element={<Home/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Transactions/Transaction.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import * as Components from "./styles";
+
+function App() {
+  const [signIn, toggle] = React.useState(true);
+  return (
+    <Components.Container>
+      <Components.SignUpContainer signinIn={signIn}>
+        <Components.Form>
+          <Components.Title>Receive</Components.Title>
+          <Components.Input type="text" placeholder="To..." />
+          <Components.Input type="text" placeholder="From..." />
+          <Components.Input type="number" placeholder="Amount" />
+          <Components.Button>Ask to Receive</Components.Button>
+        </Components.Form>
+      </Components.SignUpContainer>
+
+      <Components.SignInContainer signinIn={signIn}>
+        <Components.Form>
+          <Components.Title>Send</Components.Title>
+          <Components.Input type="text" placeholder="To..." />
+          <Components.Input type="text" placeholder="From..." disabled />
+          <Components.Input type="number" placeholder="Amount" />
+          <Components.Button>Send</Components.Button>
+        </Components.Form>
+      </Components.SignInContainer>
+
+      <Components.OverlayContainer signinIn={signIn}>
+        <Components.Overlay signinIn={signIn}>
+          <Components.LeftOverlayPanel signinIn={signIn}>
+            <Components.Title>Welcome Back!</Components.Title>
+            <Components.Paragraph>
+              Want to send some amount ?
+            </Components.Paragraph>
+            <Components.GhostButton onClick={() => toggle(true)}>
+              Send
+            </Components.GhostButton>
+          </Components.LeftOverlayPanel>
+
+          <Components.RightOverlayPanel signinIn={signIn}>
+            <Components.Title>Hello, Friend!</Components.Title>
+            <Components.Paragraph>
+              Want to receive some amount ? Follow me!!
+            </Components.Paragraph>
+            <Components.GhostButton onClick={() => toggle(false)}>
+              Receive
+            </Components.GhostButton>
+          </Components.RightOverlayPanel>
+        </Components.Overlay>
+      </Components.OverlayContainer>
+    </Components.Container>
+  );
+}
+
+export default App;
