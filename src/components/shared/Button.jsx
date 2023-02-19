723201eed44e4e029d9ffda2d4c5ef510952ae61
SirSimon162 <anmolpattnayak2016@gmail.com>
2023-02-19 19:25:22 +0530

    add: log out button

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = ({children}) => {
   return (
-    <button className="border-[2px] rounded-lg border-secondary hover:text-secondary px-[30px] py-[10px] text-white hover:border-white font-medium">
+    <button className="border-[2px] rounded-lg border-primary px-[30px] py-[10px] text-primary hover:border-base-100 font-medium bg-none hover:bg-accent hover:text-base-100 transition-all duration-[150] ease-in">
       {children}
     </button>
   );
