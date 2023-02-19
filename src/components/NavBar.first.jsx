27c4c554c096ce0f5d0478c908565aa5a2063c88
SirSimon162 <anmolpattnayak2016@gmail.com>
2023-02-19 18:47:48 +0530

    add logo

--- src/components/NavBar.first.jsx
+++ src/components/NavBar.first.jsx
@@ -9,8 +9,8 @@ const NavFirst = () => {
     <div className="shadow-md w-auto top-0 left-0 relative z-[10]">
       <div className="flex items-center justify-between sm:px-10 px-3 h-[96px] shadow-nav">
         <Link to='/' className="cursor-pointer flex items-center text-2xl">
-          <RiHandCoinLine alt="logo" className="h-[96px]" />
-          <p className="font-bold ml-[10px]">AnyMoney</p>
+        <img src="https://res.cloudinary.com/dmofs5r4h/image/upload/v1676812236/logo-02_puw7kb.png" className="h-[55px]"/>
+          <p className="font-bold ml-[10px]">PayPol</p>
         </Link>
           <Link to='/login' className="btn btn-accent btn-outline text-white sm:btn-lg lg:ml-[8px] ml-0 btn-sm">Sign In</Link>
       </div>
